7b763c83e3778618b574225e07b260e785555fbf
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-05 21:39:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
  var customerName = "bob"
 function upperCaseCustomerName() {
-   var customerName = customerName.to_uppercase
-   return customerName
+
+   return customerName.to_uppercase
 
 }
